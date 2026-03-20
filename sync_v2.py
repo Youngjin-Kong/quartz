f9e6e505abcba58c670ac3ccaeaae288b2cb49b5
Youngjin Kong <0jin.kong@gmail.com>
2026-03-20 11:00:03 +0900

    Quartz sync: Mar 20, 2026, 11:00 AM

--- sync_v2.py
+++ sync_v2.py
@@ -35,7 +35,7 @@ def sync_and_deploy():
         # 고정 제외 패턴
         ignore_func = shutil.ignore_patterns(
             '.obsidian', '.trash', 'private', '*.canvas',
-            'pen-200.pdf', '*Extra Mile Offensive Cloud Lab*', '*Exam-Report*'
+            'pen-200.pdf', '*Extra Mile Offensive Cloud Lab*', '*OSCP-OS-*'
         )
         shutil.copytree(SOURCE_VAULT, DEST_QUARTZ_CONTENT, ignore=ignore_func)
         
